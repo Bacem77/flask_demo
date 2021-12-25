c438d9721cdc22aeaf0d9514834c92dae26c0093
Bacem77 <beebenzer@gmail.com>
2021-12-26 00:58:30 +0100

    Finializing

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask,request
 import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return '<h1 style="color:RED">Hello, World! this website lets you calculate the sum of two numbers.<br> You can do that by entering an url containing the first and second parameters as routes like shown in this exemple :<br> https://flask-demo-cicd.herokuapp.com/5/5 .</h1>'
+
+@app.route("/<a>/<b>/")
+def calculate(a=None,b=None):
+    sum = "NONE"
+    if a and b:
+        sum = str((int(a)+int(b)))
+    return "<h1>The sum is "+ sum + "</h1>"
 
 def sum(a,b):
     return a+b
